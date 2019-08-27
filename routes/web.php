3bc799d56c4bca1cc98c5cc23da93084f72fca90
Gabkings <gabworks51@gmail.com>
2019-08-28 02:13:29 +0300

    first simple test

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/about', function () {
+   return "About Me";
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
---
+++ tests/Feature/AboutPageTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AboutPageTest extends TestCase
+{
+    public function testAboutPage(){
+        $res = $this->get('/about');
+
+        $res->assertStatus(200);
+        $res->assertSee("About Me");
+    }
+}
